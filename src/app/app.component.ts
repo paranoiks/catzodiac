18b53fd8541d7c73367c2aba96a5efcef23355e6
WSVosh <vladislav.boshnakov@wslimitless.com>
2017-09-23 00:09:22 +0100

    raycast working and not working again

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,11 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import * as THREE from 'three';
 import { MagicBall } from './magicball.model';
-import { Mesh, Texture, TextureLoader } from 'three';
+import {
+  Mesh, MeshBasicMaterial, MeshLambertMaterial, Texture,
+  TextureLoader
+} from 'three';
+import { current } from "codelyzer/util/syntaxKind";
 
 @Component( {
   selector : 'app-root',
@@ -23,68 +27,102 @@ export class AppComponent {
 
   public textureLoader : TextureLoader;
 
+  public catMaterials : MeshBasicMaterial[] = [];
+
+  public magicBallList : { [key : string] : MagicBall; } = {};
+
+  public magicBallsObjects = [];
+
 
   constructor() {
     this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
     this.camera.position.z = 1000;
+
     this.scene = new THREE.Scene();
 
-    this.material = new THREE.MeshBasicMaterial(0x00ff00);
-    //this.material = new THREE.MeshLambertMaterial(texture);
-
-    this.createSpheres();
-
     this.textureLoader = new TextureLoader();
-
-    this.textureLoader.load(
-      '../assets/img/cat1.jpeg',
-      (texture) =>  {
-
-
-      });
+    let texturesLoaded = 0;
+    for ( let i = 1; i < 5; i++ ) {
+      this.textureLoader.load(
+        '../assets/img/cat' + i + '.jpg',
+        ( texture ) => {
+          this.catMaterials.push( new MeshBasicMaterial( { map : texture } ) );
+          texturesLoaded++;
+          if ( texturesLoaded === 4 ) {
+            this.createSpheres();
+          }
+        } );
     }
+    document.addEventListener( 'mousedown', this.onDocumentMouseDown, false );
+  }
 
-    createSpheres(){
-      const geometry = new THREE.SphereGeometry( 50, 15, 15 );
 
-      const bigSphereRadius = 500;
-      let currentMesh : Mesh = null;
+  onDocumentMouseDown = ( e ) => {
 
-      let rad : number;
-      let x : number;
-      let y : number;
+    console.log( this.renderer );
+    e.preventDefault();
 
-      for ( let n = 1; n <= 12; n++ ) {
-        currentMesh = new THREE.Mesh( geometry, this.material );
+    let raycaster = new THREE.Raycaster();
+    let mouse = new THREE.Vector2();
+    mouse.x = ( e.clientX / this.renderer.domElement.clientWidth ) * 2 - 1;
+    mouse.y = -( e.clientY / this.renderer.domElement.clientHeight ) * 2 + 1;
 
-        rad = n * 30 * (Math.PI / 180);
+    raycaster.setFromCamera( mouse, this.camera );
 
-        x = Math.cos( rad ) * bigSphereRadius;
-        y = Math.sin( rad ) * bigSphereRadius;
+    console.log(this.magicBallsObjects.length);
+    let intersects = raycaster.intersectObjects( this.magicBallsObjects );
 
-        currentMesh.position.set( Math.cos( rad ) * bigSphereRadius, Math.sin( rad ) * bigSphereRadius, 200 );
-
-        this.scene.add( currentMesh );
-
-        this.magicBalls.push( new MagicBall( currentMesh ) );
-      }
+    console.log(intersects.length);
+    if ( intersects.length > 0 ) {
+      let clickedMagicBall = this.magicBallList[intersects[0].object.uuid];
+      console.log( clickedMagicBall );
     }
+  };
 
-    ngAfterViewInit()
-    {
-      this.renderer.setSize( window.innerWidth, window.innerHeight );
-      this.rendererContainer.nativeElement.appendChild( this.renderer.domElement );
-      this.animate();
-    }
+  createSpheres() {
+    const geometry = new THREE.SphereGeometry( 50, 15, 15 );
 
-    animate()
-    {
-      window.requestAnimationFrame( () => this.animate() );
+    const bigSphereRadius = 500;
+    let currentMesh : Mesh = null;
 
-      this.renderer.render( this.scene, this.camera );
+    let rad : number;
+    let x : number;
+    let y : number;
 
-      for ( let i = 0; i < this.magicBalls.length; i++ ) {
-        this.magicBalls[ i ].animate();
-      }
+    for ( let n = 1; n <= 12; n++ ) {
+      currentMesh = new THREE.Mesh(
+        geometry,
+        this.catMaterials[ Math.floor( Math.random() * 3 ) ]
+      );
+
+      rad = n * 30 * (Math.PI / 180);
+
+      x = Math.cos( rad ) * bigSphereRadius;
+      y = Math.sin( rad ) * bigSphereRadius;
+
+      currentMesh.position.set( Math.cos( rad ) * bigSphereRadius, Math.sin( rad ) * bigSphereRadius, 200 );
+
+      this.scene.add( currentMesh );
+
+      this.magicBallsObjects.push(currentMesh);
+      this.magicBalls.push( new MagicBall( currentMesh ) );
+      this.magicBallList[ currentMesh.uuid ] = this.magicBalls[ this.magicBalls.length - 1 ];
     }
   }
+
+  ngAfterViewInit() {
+    this.renderer.setSize( window.innerWidth, window.innerHeight );
+    this.rendererContainer.nativeElement.appendChild( this.renderer.domElement );
+    this.animate();
+  }
+
+  animate() {
+    window.requestAnimationFrame( () => this.animate() );
+
+    this.renderer.render( this.scene, this.camera );
+
+    for ( let i = 0; i < this.magicBalls.length; i++ ) {
+      this.magicBalls[ i ].animate();
+    }
+  }
+}
--- src/app/magicball.model.ts
+++ src/app/magicball.model.ts
@@ -4,13 +4,26 @@ export class MagicBall {
 
   public mainMesh: Mesh;
   public rotationSpeed: number;
+  public rotationSpeedDefault: number;
+  public rotating: boolean;
 
   constructor (mesh: Mesh) {
+    this.rotating = true;
     this.mainMesh = mesh;
-    this.rotationSpeed = Math.random() * 3;
+    this.rotationSpeed = this.rotationSpeedDefault = Math.random() * 0.4;
   }
 
   animate() {
     this.mainMesh.rotateY(this.rotationSpeed);
+  }
+
+  public toggleRotation(){
+    this.rotating = !this.rotating;
+    if (this.rotating){
+      this.rotationSpeed = this.rotationSpeedDefault;
+    }
+    else {
+      this.rotationSpeed = 0;
+    }
   }
 }
